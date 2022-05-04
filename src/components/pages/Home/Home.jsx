d6b92ff19828d582717b2a562e27997d92d1e6cd
prasouk-ph <prasouk.ph@gmail.com>
2022-05-04 17:34:43 +0200

    Add controlled form

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import ReactModal from 'react-modal';
 import Dropdown from 'react-dropdown';
@@ -8,9 +8,66 @@ import './Home.css';
 
 
 function Home() {
-  const [birthDate, setBirthDate] = useState();
-  const [startDate, setStartDate] = useState();
+  const initialFormData = {
+    firstname: '',
+    lastname: '',
+    dateofbirth: '',
+    startdate: '',
+    street: '',
+    city: '',
+    state: '',
+    zipcode: '',
+    departments: ''
+  }
+
+  const [formData, setFormData] = useState(initialFormData)
+  const [birthDate, setBirthDate] = useState('');
+  const [startDate, setStartDate] = useState('');
   const [modalIsOpen, setIsOpen] = useState(false);
+
+
+  function handleTextInputChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    })
+  }
+
+
+  // can't use in onChange because will update only after 2 changes
+  function handleDateInputChange(inputName, inputValue) {
+    setFormData({
+      ...formData,
+      [inputName]: inputValue
+    })
+  }
+
+  useEffect(() => {
+    const birthDateFormatted = new Date(birthDate).toLocaleDateString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"})
+    handleDateInputChange('dateofbirth', birthDateFormatted)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [birthDate])
+
+  useEffect(() => {
+    const startDateFormatted = new Intl.DateTimeFormat('en-US', {month: "2-digit", day: "2-digit", year: "numeric"}).format(startDate); // to avoid invalid date
+    handleDateInputChange('startdate', startDateFormatted)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [startDate])
+
+
+  function handleDropDownChange(event, dropdownName) {
+    setFormData({
+      ...formData,
+      [dropdownName]: event.label
+    })
+  }
+
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(formData)
+  }
+
 
   function openModal() {
     setIsOpen(true);
@@ -36,73 +93,73 @@ function Home() {
   ReactModal.setAppElement(document.getElementById('root'));
 
   const statesOptions = [
-    { value: 'AL', label: 'Alabama' },
+    { value: 'AL', label: 'Alabama', className: 'state' },
     { value: 'AK', label: 'Alaska' },
-    { value: 'AS', label: 'American Samoa' },
-    { value: 'AZ', label: 'Arizona' },
-    { value: 'AR', label: 'Arkansas' },
-    { value: 'CA', label: 'California' },
-    { value: 'CO', label: 'Colorado' },
-    { value: 'CT', label: 'Connecticut' },
-    { value: 'DE', label: 'Delaware' },
-    { value: 'DC', label: 'District Of Columbia' },
-    { value: 'FM', label: 'Federated States Of Micronesia' },
-    { value: 'FL', label: 'Florida' },
-    { value: 'GA', label: 'Georgia' },
+    { value: 'AS', label: 'American Samoa', className: 'state' },
+    { value: 'AZ', label: 'Arizona', className: 'state' },
+    { value: 'AR', label: 'Arkansas', className: 'state' },
+    { value: 'CA', label: 'California', className: 'state' },
+    { value: 'CO', label: 'Colorado', className: 'state' },
+    { value: 'CT', label: 'Connecticut', className: 'state' },
+    { value: 'DE', label: 'Delaware', className: 'state' },
+    { value: 'DC', label: 'District Of Columbia', className: 'state' },
+    { value: 'FM', label: 'Federated States Of Micronesia', className: 'state' },
+    { value: 'FL', label: 'Florida', className: 'state' },
+    { value: 'GA', label: 'Georgia', className: 'state' },
     { value: 'GU', label: 'Guam' },
     { value: 'HI', label: 'Hawaii' },
     { value: 'ID', label: 'Idaho' },
-    { value: 'IL', label: 'Illinois' },
-    { value: 'IN', label: 'Indiana' },
+    { value: 'IL', label: 'Illinois', className: 'state' },
+    { value: 'IN', label: 'Indiana', className: 'state' },
     { value: 'IA', label: 'Iowa' },
     { value: 'KS', label: 'Kansas' },
-    { value: 'KY', label: 'Kentucky' },
-    { value: 'LA', label: 'Louisiana' },
+    { value: 'KY', label: 'Kentucky', className: 'state' },
+    { value: 'LA', label: 'Louisiana', className: 'state' },
     { value: 'ME', label: 'Maine' },
-    { value: 'MH', label: 'Marshall Islands' },
-    { value: 'MD', label: 'Maryland' },
-    { value: 'MA', label: 'Massachusetts' },
-    { value: 'MI', label: 'Michigan' },
-    { value: 'MN', label: 'Minnesota' },
-    { value: 'MS', label: 'Mississippi' },
-    { value: 'MO', label: 'Missouri' },
-    { value: 'MT', label: 'Montana' },
-    { value: 'NE', label: 'Nebraska' },
+    { value: 'MH', label: 'Marshall Islands', className: 'state' },
+    { value: 'MD', label: 'Maryland', className: 'state' },
+    { value: 'MA', label: 'Massachusetts', className: 'state' },
+    { value: 'MI', label: 'Michigan', className: 'state' },
+    { value: 'MN', label: 'Minnesota', className: 'state' },
+    { value: 'MS', label: 'Mississippi', className: 'state' },
+    { value: 'MO', label: 'Missouri', className: 'state' },
+    { value: 'MT', label: 'Montana', className: 'state' },
+    { value: 'NE', label: 'Nebraska', className: 'state' },
     { value: 'NV', label: 'Nevada' },
-    { value: 'NH', label: 'New Hampshire' },
-    { value: 'NJ', label: 'New Jersey' },
-    { value: 'NM', label: 'New Mexico' },
-    { value: 'NY', label: 'New York' },
-    { value: 'NC', label: 'North Carolina' },
-    { value: 'ND', label: 'North Dakota' },
-    { value: 'MP', label: 'Northern Mariana Islands' },
+    { value: 'NH', label: 'New Hampshire', className: 'state' },
+    { value: 'NJ', label: 'New Jersey', className: 'state' },
+    { value: 'NM', label: 'New Mexico', className: 'state' },
+    { value: 'NY', label: 'New York', className: 'state' },
+    { value: 'NC', label: 'North Carolina', className: 'state' },
+    { value: 'ND', label: 'North Dakota', className: 'state' },
+    { value: 'MP', label: 'Northern Mariana Islands', className: 'state' },
     { value: 'OH', label: 'Ohio' },
-    { value: 'OK', label: 'Oklahoma' },
+    { value: 'OK', label: 'Oklahoma', className: 'state' },
     { value: 'OR', label: 'Oregon' },
     { value: 'PW', label: 'Palau' },
-    { value: 'PA', label: 'Pennsylvania' },
-    { value: 'PR', label: 'Puerto Rico' },
-    { value: 'RI', label: 'Rhode Island' },
-    { value: 'SC', label: 'South Carolina' },
-    { value: 'SD', label: 'South Dakota' },
-    { value: 'TN', label: 'Tennessee' },
+    { value: 'PA', label: 'Pennsylvania', className: 'state' },
+    { value: 'PR', label: 'Puerto Rico', className: 'state' },
+    { value: 'RI', label: 'Rhode Island', className: 'state' },
+    { value: 'SC', label: 'South Carolina', className: 'state' },
+    { value: 'SD', label: 'South Dakota', className: 'state' },
+    { value: 'TN', label: 'Tennessee', className: 'state' },
     { value: 'TV', label: 'Texas' },
     { value: 'UT', label: 'Utah' },
-    { value: 'VT', label: 'Vermont' },
-    { value: 'VI', label: 'Virgin Islands' },
-    { value: 'VA', label: 'Virginia' },
-    { value: 'WA', label: 'Washington' },
-    { value: 'WV', label: 'West Virginia' },
-    { value: 'WI', label: 'Wisconsin' },
-    { value: 'WY', label: 'Wyoming' },
+    { value: 'VT', label: 'Vermont', className: 'state' },
+    { value: 'VI', label: 'Virgin Islands', className: 'state' },
+    { value: 'VA', label: 'Virginia', className: 'state' },
+    { value: 'WA', label: 'Washington', className: 'state' },
+    { value: 'WV', label: 'West Virginia', className: 'state' },
+    { value: 'WI', label: 'Wisconsin', className: 'state' },
+    { value: 'WY', label: 'Wyoming', className: 'state' },
   ]
 
   const departmentsOptions = [
-    { value: 'sales', label: 'Sales' },
-    { value: 'marketing', label: 'Marketing' },
-    { value: 'engineering', label: 'Engineering' },
-    { value: 'human Resources', label: 'Human Resources' },
-    { value: 'legal', label: 'Legal' },
+    { value: 'sales', label: 'Sales', className: 'departments' },
+    { value: 'marketing', label: 'Marketing', className: 'departments' },
+    { value: 'engineering', label: 'Engineering', className: 'departments' },
+    { value: 'human Resources', label: 'Human Resources', className: 'departments' },
+    { value: 'legal', label: 'Legal', className: 'departments' },
   ]
 
   return (
@@ -112,19 +169,22 @@ function Home() {
       </header>
 
       <main className='home-main'>
-        <form action="#" id='create-employee'>
+        <form action="#" id='create-employee' onSubmit={handleSubmit}>
           <div className='employee-data'>
-            <label htmlFor="first-name">First Name</label>
-            <input type="text" name='first-name' id='first-name' />
+            <label htmlFor="firstname">First Name</label>
+            <input type="text" name='firstname' id='firstname' onChange={handleTextInputChange} />
 
-            <label htmlFor="last-name">Last Name</label>
-            <input type="text" name='last-name' id='last-name' />
+            <label htmlFor="lastname">Last Name</label>
+            <input type="text" name='lastname' id='lastname' onChange={handleTextInputChange} />
 
-            <label htmlFor="date-of-birth">Date of Birth</label>
+            <label htmlFor="dateofbirth">Date of Birth</label>
             <DatePicker
-              id='date-of-birth'
+              id='dateofbirth'
+              name='dateofbirth'
               selected={birthDate}
-              onChange={(date) => setBirthDate(date)}
+              onChange={(date) => {
+                setBirthDate(date)
+              }}
               peekNextMonth
               showMonthDropdown
               showYearDropdown
@@ -133,11 +193,14 @@ function Home() {
               placeholderText="DD/MM/YYYY"
             />
 
-            <label htmlFor="start-date">Start Date</label>
+            <label htmlFor="startdate">Start Date</label>
             <DatePicker
-              id='start-date'
+              id='startdate'
+              name='startdate'
               selected={startDate}
-              onChange={(date) => setStartDate(date)}
+              onChange={(date) => {
+                setStartDate(date)
+              }}
               peekNextMonth
               showMonthDropdown
               showYearDropdown
@@ -151,34 +214,34 @@ function Home() {
             <legend>Address</legend>
 
             <label htmlFor="street">Street</label>
-            <input id="street" type="text" />
+            <input name="street" id="street" type="text" onChange={handleTextInputChange} />
 
             <label htmlFor="city">City</label>
-            <input id="city" type="text" />
+            <input name="city" id="city" type="text" onChange={handleTextInputChange} />
 
             <label htmlFor="state">State</label>
-            <Dropdown id="state" options={statesOptions} placeholder="Select an option" />
+            <Dropdown name="state" id="state" options={statesOptions} placeholder="Select an option" onChange={(event) => handleDropDownChange(event, 'state')} />
 
-            <label htmlFor="zip-code">Zip Code</label>
-            <input id="zip-code" type="number" />
+            <label htmlFor="zipcode">Zip Code</label>
+            <input name="zipcode" id="zipcode" type="number" onChange={handleTextInputChange} />
           </fieldset>
 
           <div className='employee-department'>
             <label htmlFor="departments">Department</label>
-            <Dropdown id="departments" options={departmentsOptions} placeholder="Select an option" />
+            <Dropdown name="departments" id="departments" options={departmentsOptions} placeholder="Select an option" onChange={(event) => handleDropDownChange(event, 'departments')} />
           </div>
-        </form>
         
-        <button type='submit' onClick={openModal}>Save</button>
-        <ReactModal
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          style={customStyles}
-          contentLabel="Form Submit Modal"
-        >
-          <p>Employee Created!</p>
-          <button className="button-close" onClick={closeModal}>Close</button>
-        </ReactModal>
+          <button type='submit' onClick={openModal} >Save</button>
+          <ReactModal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Form Submit Modal"
+            >
+            <p>Employee Created!</p>
+            <button className="button-close" onClick={closeModal}>Close</button>
+          </ReactModal>
+        </form>
       </main>
     </div>
   );
